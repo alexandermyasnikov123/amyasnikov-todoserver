d24090d37ee23d097ddb54b262fbb62b65023125
alexander <dart69@vk.com>
2024-11-04 22:01:05 +0300

    Add Cors support for the controller. Gui tests available at "https://todo.dunice-testing.com/?baseURL=https://todoapp-gls7.onrender.com"

--- src/main/java/net/dunice/todo/controllers/TodosController.java
+++ src/main/java/net/dunice/todo/controllers/TodosController.java
@@ -15,6 +15,7 @@ import net.dunice.todo.entities.TodoEntity;
 import net.dunice.todo.services.TodosService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
+@CrossOrigin
 @RestController
 @RequestMapping(path = "v1/todo")
 @RequiredArgsConstructor
